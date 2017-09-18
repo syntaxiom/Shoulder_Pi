52a43f47ed8c4715a355701b7aa08eeefa28f553
Sean Lester-Wilson <smlwcg@gmail.com>
2017-09-17 21:38:59 -0700

    Figuring it out!

---
+++ ver0.4/main3.c
@@ -0,0 +1,89 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+
+// 'global' variables to store screen info
+char *fbp = 0;
+struct fb_var_screeninfo vinfo;
+struct fb_fix_screeninfo finfo;
+
+// helper function to 'plot' a pixel in given color
+void put_pixel(int x, int y, int c)
+{
+    // calculate the pixel's byte offset inside the buffer
+    unsigned int pix_offset = x + y * finfo.line_length;
+
+    // now this is about the same as 'fbp[pix_offset] = value'
+    fbp[pix_offset] = c;
+
+}
+
+// helper function for drawing - no more need to go mess with
+// the main function when just want to change what to draw...
+void draw() {
+
+    int x, y;
+
+    for (y = 0; y < vinfo.yres; y++) {
+        for (x = 0; x < vinfo.xres; x++) {
+
+            // color based on the 16th of the screen width
+            int c = 16 * x / vinfo.xres;
+
+            // call the helper function
+            put_pixel(x, y, c);
+
+        }
+    }
+
+}
+
+// application entry point
+int main(int argc, char* argv[])
+{
+
+    int fbfd = 0;
+    struct fb_var_screeninfo orig_vinfo;
+    long int screensize = 0;
+
+
+    // Open the file for reading and writing
+    fbfd = open("/dev/fb0", O_RDWR);
+
+    // Get variable screen information
+    ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo);
+
+    // Store for reset (copy vinfo to vinfo_orig)
+    memcpy(&orig_vinfo, &vinfo, sizeof(struct fb_var_screeninfo));
+
+    // Change variable info
+    vinfo.bits_per_pixel = 8;
+    ioctl(fbfd, FBIOPUT_VSCREENINFO, &vinfo);
+
+    // Get fixed screen information
+    ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo);
+
+    // map fb to user mem 
+    screensize = vinfo.xres * vinfo.yres;
+    fbp = (char*)mmap(0, 
+              screensize, 
+              PROT_READ | PROT_WRITE, 
+              MAP_SHARED, 
+              fbfd, 
+              0);
+
+    draw();
+    sleep(5);
+
+    // cleanup
+    munmap(fbp, screensize);
+    ioctl(fbfd, FBIOPUT_VSCREENINFO, &orig_vinfo);
+    close(fbfd);
+
+    return 0;
+
+}
--- ver0.4/main3.s
+++ ver0.4/main3.s
@@ -9,7 +9,7 @@
 	.eabi_attribute 30, 6
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
-	.file	"main.c"
+	.file	"main3.c"
 	.global	fbp
 	.bss
 	.align	2
@@ -37,21 +37,21 @@ put_pixel:
 	str	r0, [fp, #-16]	// (fp - 16) = x
 	str	r1, [fp, #-20]	// (fp - 20) = y
 	str	r2, [fp, #-24]	// (fp - 24) = c
-	ldr	r3, .L2		// needed for C compilation
+	ldr	r3, .L2		// r3 = finfo
 	ldr	r3, [r3, #44]	// r3 = finfo.line_length
 	ldr	r2, [fp, #-20]	// r2 = y
 	mul	r2, r2, r3	// r2 = y * finfo.line_length
 	ldr	r3, [fp, #-16]	// r3 = x
 	add	r3, r2, r3	// r3 = x + (r2 = y * finfo.line_length)
 	str	r3, [fp, #-8]
-	ldr	r3, .L2+4
+	ldr	r3, .L2+4	// r3 = fbp
 	ldr	r2, [r3]
 	ldr	r3, [fp, #-8]
-	add	r3, r2, r3	// fbp + pix_offset
-	ldr	r2, [fp, #-24]	// r2 = c
-	uxtb	r2, r2		// Extends r2 (c) to 32-bit value
+	add	r3, r2, r3
+	ldr	r2, [fp, #-24]
+	uxtb	r2, r2
 	strb	r2, [r3]
-	nop			// Why is this here? Padding?
+	nop
 	add	sp, fp, #0
 	@ sp needed
 	ldr	fp, [sp], #4
@@ -97,7 +97,7 @@ draw:
 	ldr	r2, [fp, #-16]
 	ldr	r1, [fp, #-12]
 	ldr	r0, [fp, #-8]
-	bl	put_pixel	// Parameters for `put_pixel` are r2, r1, and r0
+	bl	put_pixel
 	ldr	r3, [fp, #-8]
 	add	r3, r3, #1
 	str	r3, [fp, #-8]
@@ -112,8 +112,7 @@ draw:
 	str	r3, [fp, #-12]
 .L5:
 	ldr	r3, .L9
-	ldr	r3, [r3, #4]
-	lsr	r2, r3, #1
+	ldr	r2, [r3, #4]
 	ldr	r3, [fp, #-12]
 	cmp	r2, r3
 	bhi	.L8
@@ -130,30 +129,6 @@ draw:
 	.align	2
 .LC0:
 	.ascii	"/dev/fb0\000"
-	.align	2
-.LC1:
-	.ascii	"Error: cannot open framebuffer device.\000"
-	.align	2
-.LC2:
-	.ascii	"The framebuffer device was opened successfully.\000"
-	.align	2
-.LC3:
-	.ascii	"Error reading variable information.\000"
-	.align	2
-.LC4:
-	.ascii	"Original %dx%d, %dbpp\012\000"
-	.align	2
-.LC5:
-	.ascii	"Error setting variable information.\000"
-	.align	2
-.LC6:
-	.ascii	"Error reading fixed information.\000"
-	.align	2
-.LC7:
-	.ascii	"Failed to mmap.\000"
-	.align	2
-.LC8:
-	.ascii	"Error re-setting variable information.\000"
 	.text
 	.align	2
 
@@ -174,70 +149,34 @@ main:
 	str	r3, [fp, #-8]
 	mov	r3, #0
 	str	r3, [fp, #-12]
-	mov	r1, #2		// O_RDWR; opcode for reading and writing = 2
-	ldr	r0, .L21	// "dev/fb0"
-	bl	open		// Parameters for `open` are r1 and r0
+	mov	r1, #2
+	ldr	r0, .L13
+	bl	open
 	str	r0, [fp, #-8]
-	ldr	r3, [fp, #-8]
-	cmp	r3, #0
-	bne	.L12
-	ldr	r0, .L21+4
-	bl	puts
-	mov	r3, #1
-	b	.L20
-.L12:
-	ldr	r0, .L21+8
-	bl	puts
-	ldr	r2, .L21+12
+	ldr	r2, .L13+4
 	mov	r1, #17920
 	ldr	r0, [fp, #-8]
 	bl	ioctl
-	mov	r3, r0
-	cmp	r3, #0
-	beq	.L14
-	ldr	r0, .L21+16
-	bl	puts
-.L14:
-	ldr	r3, .L21+12
-	ldr	r1, [r3]
-	ldr	r3, .L21+12
-	ldr	r2, [r3, #4]
-	ldr	r3, .L21+12
-	ldr	r3, [r3, #24]
-	ldr	r0, .L21+20
-	bl	printf
-	ldr	r2, .L21+12
+	ldr	r2, .L13+4
 	sub	r3, fp, #172
 	mov	r1, r2
 	mov	r2, #160
 	mov	r0, r3
 	bl	memcpy
-	ldr	r3, .L21+12
+	ldr	r3, .L13+4
 	mov	r2, #8
 	str	r2, [r3, #24]
-	ldr	r2, .L21+12
-	ldr	r1, .L21+24
+	ldr	r2, .L13+4
+	ldr	r1, .L13+8
 	ldr	r0, [fp, #-8]
 	bl	ioctl
-	mov	r3, r0
-	cmp	r3, #0
-	beq	.L15
-	ldr	r0, .L21+28
-	bl	puts
-.L15:
-	ldr	r2, .L21+32
-	ldr	r1, .L21+36
+	ldr	r2, .L13+12
+	ldr	r1, .L13+16
 	ldr	r0, [fp, #-8]
 	bl	ioctl
-	mov	r3, r0
-	cmp	r3, #0
-	beq	.L16
-	ldr	r0, .L21+40
-	bl	puts
-.L16:
-	ldr	r3, .L21+12
+	ldr	r3, .L13+4
 	ldr	r3, [r3]
-	ldr	r2, .L21+12
+	ldr	r2, .L13+4
 	ldr	r2, [r2, #4]
 	mul	r3, r2, r3
 	str	r3, [fp, #-12]
@@ -251,21 +190,12 @@ main:
 	mov	r0, #0
 	bl	mmap
 	mov	r2, r0
-	ldr	r3, .L21+44
+	ldr	r3, .L13+20
 	str	r2, [r3]
-	ldr	r3, .L21+44
-	ldr	r3, [r3]
-	cmn	r3, #1
-	bne	.L17
-	ldr	r0, .L21+48
-	bl	puts
-	b	.L18
-.L17:
 	bl	draw
 	mov	r0, #5
 	bl	sleep
-.L18:
-	ldr	r3, .L21+44
+	ldr	r3, .L13+20
 	ldr	r3, [r3]
 	ldr	r2, [fp, #-12]
 	mov	r1, r2
@@ -273,40 +203,25 @@ main:
 	bl	munmap
 	sub	r3, fp, #172
 	mov	r2, r3
-	ldr	r1, .L21+24
+	ldr	r1, .L13+8
 	ldr	r0, [fp, #-8]
 	bl	ioctl
-	mov	r3, r0
-	cmp	r3, #0
-	beq	.L19
-	ldr	r0, .L21+52
-	bl	puts
-.L19:
 	ldr	r0, [fp, #-8]
 	bl	close
 	mov	r3, #0
-.L20:
 	mov	r0, r3
 	sub	sp, fp, #4
 	@ sp needed
 	pop	{fp, pc}
-.L22:
+.L14:
 	.align	2
-.L21:
+.L13:
 	.word	.LC0
-	.word	.LC1
-	.word	.LC2
 	.word	vinfo
-	.word	.LC3
-	.word	.LC4
 	.word	17921
-	.word	.LC5
 	.word	finfo
 	.word	17922
-	.word	.LC6
 	.word	fbp
-	.word	.LC7
-	.word	.LC8
 	.size	main, .-main
 	.ident	"GCC: (Raspbian 6.3.0-18+rpi1) 6.3.0 20170516"
 	.section	.note.GNU-stack,"",%progbits
