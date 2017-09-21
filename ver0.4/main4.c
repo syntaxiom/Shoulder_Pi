10a28896085f5d496441bf561ef6b965a37f0c69
Sean Lester-Wilson <smlwcg@gmail.com>
2017-09-21 13:10:33 -0700

    PC to Pi

---
+++ ver0.4/main4.c
@@ -0,0 +1,61 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+
+// 'global' variables to store screen info
+char *fbp = 0;
+struct fb_var_screeninfo vinfo;
+struct fb_fix_screeninfo finfo;
+
+void put_pixel_RGB24(int x, int y, int r, int g, int b)
+{
+    // calculate the pixel's byte offset inside the buffer
+    // note: x * 3 as every pixel is 3 consecutive bytes
+    unsigned int pix_offset = x * 3 + y * finfo.line_length;
+
+    // now this is about the same as 'fbp[pix_offset] = value'
+    *((char*)(fbp + pix_offset)) = b;
+    *((char*)(fbp + pix_offset + 1)) = g;
+    *((char*)(fbp + pix_offset + 2)) = r;
+}
+
+int main(int argc, char* argv[])
+{
+    int fbfd = 0;
+    struct fb_var_screeninfo orig_vinfo;
+    long int screensize = 0;
+
+    // Open the file for reading and writing
+    fbfd = open("/dev/fb0", O_RDWR);
+
+    // Get variable screen information
+    ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo);
+
+    // Store for reset (copy vinfo to vinfo_orig)
+    memcpy(&orig_vinfo, &vinfo, sizeof(struct fb_var_screeninfo));
+
+    // Get fixed screen information
+    ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo);
+
+    // map fb to user mem 
+    screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+    fbp = (char*)mmap(0, 
+              screensize, 
+              PROT_READ | PROT_WRITE, 
+              MAP_SHARED, 
+              fbfd, 
+              0);
+    put_pixel_RGB24(0, 0, 255, 255, 255);
+    sleep(5);
+
+    // cleanup
+    munmap(fbp, screensize);
+    ioctl(fbfd, FBIOPUT_VSCREENINFO, &orig_vinfo);
+    close(fbfd);
+    return 0;
+}
