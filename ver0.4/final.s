38a9bd27a6e7e00de85590027f7a3675628588a8
Sean Lester-Wilson <smlwcg@gmail.com>
2017-09-28 13:21:24 -0700

    Keep going

--- ver0.4/final.s
+++ ver0.4/final.s
@@ -7,16 +7,29 @@ main:
 	STR	R0, [SP]	// SP+0 = "/dev/fb0"
 	MOV	R1, #2		// R1 = 2 (Opcode for O_RDWR)
 	BL	open		// Parameters: R0--R1
+	LDR	R0, [SP]	// R0 = "/dev/fb0"
 	LDR	R1, =opcodes	// R1 = 17920 (Opcode for FPIOGET_VSCREENINFO)
 	LDR	R2, =latch+8	// R2 = vinfo
 	BL	ioctl		// Parameters: R0--R2
+	LDR	R0, [SP]	// R0 = "/dev/fb0"
 	MOV	R1, #8		// R1 = 8
 	STR	R1, [R2, #24]	// vinfo.bits_per_pixel = 8
 	LDR	R1, =opcodes+4	// R1 = 17921 (Opcode for FBIOPUT_VSCREENINFO)
 	BL	ioctl		// Parameters: R0--R2
+	LDR	R0, [SP]	// R0 = "/dev/fb0"
 	LDR	R1, =opcodes+8	// R1 = 17922 (Opcode for FBIOGET_FSCREENINFO)
 	LDR	R2, =latch+12	// R2 = finfo
 	BL	ioctl		// Parameters: R0--R2
+	MOV	R0, #0		// R0 = 0
+	LDR	R1, =latch+8	// R1 = vinfo
+	LDR	R1, [R1]	// R1 = vinfo.xres
+	LDR	R2, [R1, #4]	// R2 = vinfo.yres
+	MUL	R1, R1, R2	// R1 = vinfo.xres * vinfo.yres
+	MOV	R2, #3		// R2 = 3 (Opcode for PROT_READ | PROT_WRITE)
+	MOV	R3, #1		// R3 = 1 (Opcode for MAP_SHARED)
+	MOV	R4, #0		// R4 = 0
+	STR	R4, [SP, #4]	// SP+4 = 0
+	BL	mmap		// Parameters: R0--R3, SP--SP+4
 	POP	{PC}
 
 /* Parameters:
