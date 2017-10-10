0a8bd6f17682367136ce888d80e9228c21f3d0cf
syntaxiom <smlwcg@gmail.com>
2017-10-10 16:14:44 -0700

    I can call LDR R0, =LATCH then LDR R0, [R0, #offset] to access individual values

--- ver0.5/game3.s
+++ ver0.5/game3.s
@@ -7,33 +7,15 @@ fbp:
 	.space	4
 	.comm	vinfo,160,4
 	.comm	finfo,68,4
-
-	/* R0 = x, R1 = y, R2 = color */
-	.text
-	.align	4
-	.global	put_pixel
-put_pixel:
-	MOV	R3, #3		// R3 = 3 (bytes per pixel)
-	AND	R4, R0, #3 	// R4 = R0 mod 4
-	MUL	R0, R0, R3	// R0 = x * 3
-	LDR	R3, [SP, #12]	// R3 = finfo.line_length
-	MUL	R1, R1, R3	// R1 = y * finfo.line_length
-	ADD	R1, R0, R1	// R1 = x * 3 + y * finfo.line_length = pix_offset
-	LDR	R0, LATCH+20	// R0 -> fbp
-	LDR	R0, [R0]	// R0 = fbp (dereferenced)
-	ADD	R0, R0, R1	// R0 = fbp + pix_offset
-	ADD	R0, R0, R4	// R0 = fbp + pix_offset + (x mod 4)
-	STR	R2, [R0]	// R0 = color (RGB)
-	MOV	PC, LR
 	
 	.text
 	.align	2
 	.global	main
 main:
-	LDR	R0, LATCH	// R0 -> .L6 -> "/dev/fb0\000"
+	LDR	R0, LATCH	// R0 -> LATCH+0 -> "/dev/fb0\000"
 	MOV	R1, #2		// R1 = 2 (Opcode for O_RDWR)
 	BL	open		// Parameters: R0--R1
-	STR	R0, [SP]	// SP = open(...)
+	STR	R0, [SP]	// SP = open("/dev/fb0\000")
 	LDR	R1, LATCH+12	// R1 = 17920 (Opcode for FBIOGET_VSCREENINFO)
 	LDR	R2, LATCH+4	// R2 -> LATCH+4 -> vinfo (Changed)
 	BL	ioctl		// Parameters: R0--R2
@@ -62,18 +44,22 @@ main:
 	LDR	R0, LATCH+8	// R0 -> finfo
 	LDR	R0, [R0, #44]	// R0 = finfo.line_length (dereferenced)
 	STR	R0, [SP, #12]	// SP+12 = finfo.line_length
-	MOV	R0, #800
-	MOV	R1, #800
-	LDR	R2, =0xFFFF00FF
-	BL	put_pixel
-	
+	NOP
+	LDR	R0, [SP]	// R0 = open("/dev/fb0\000")
+	STR	R0, [SP, #16]	// SP+16 = open("/dev/fb0\000")
+	NOP
+	//BL	show_image
+	LDR	R0, =LATCH
+	LDR	R0, [R0, #12]
+	NOP
+
 	.global main2
-main2:	
+main2:
 	LDR	R0, LATCH+20	// R0 -> LATCH+20 = fbp
 	LDR	R0, [R0]	// R0 = fbp (dereferenced)
-	LDR	R1, [SP, #8]	// SP+8 = screensize
+	LDR	R1, [SP, #8]	// R1 = screensize
 	BL	munmap		// Parameters: R0--R1
-	LDR	R0, [SP]	// R0 -> SP = open(...)
+	LDR	R0, [SP, #16]	// R0 -> SP+16 = open("/dev/fb0\000")
 	BL	close		// Parameters: R0
 	MOV	R0, #0		// R0 = 0 (return code)
 	MOVAL	R7, #1		// R7 = 1 (exit syscall)
@@ -81,17 +67,18 @@ main2:
 
 	.align	2
 LATCH:
-	.word	FILE
+	.word	FRAMEBUFFER
 	.word	vinfo
 	.word	finfo
 	.word	17920
 	.word	17922
 	.word	fbp
+	.word	IMAGE
 
 	.section	.rodata
 	.align	2
-FILE:
+FRAMEBUFFER:
 	.ascii	"/dev/fb0\000"
 
 IMAGE:
-	.ascii "test_image.bin"
+	.ascii	"/home/pi/Desktop/shoulder/image.bin\000"
