e06948eb5f55fb7b42348b6c2a30b865858af0bb
syntaxiom <smlwcg@gmail.com>
2017-10-10 22:13:18 -0700

    I might be figuring out mmap

--- ver0.5/game5.s
+++ ver0.5/game5.s
@@ -7,44 +7,17 @@ fbp:
 	.space	4
 	.comm	vinfo,160,4
 	.comm	finfo,68,4
-
-	/* R0 = x, R1 = y, R2 = r, R3 = g, R4 = b */
-	.text
-	.align	2
-	.global	put_pixel
-put_pixel:
-	MOV	R5, #3		// R5 = 3 (bytes per pixel)
-	MUL	R0, R0, R5	// R0 = x * 3
-	LDR	R5, LATCH+8	// R5 -> finfo
-	LDR	R5, [R5, #44]	// R5 = fino+44 (dereferenced) ==> finfo.line_length
-	MUL	R1, R1, R5	// R1 = y * finfo.line_length
-	ADD	R1, R0, R1	// R1 = x * 3 + y * finfo.line_length = pix_offset
-	LDR	R0, LATCH+20	// R0 -> fbp
-	LDR	R0, [R0]	// R0 = fbp (dereferenced)
-	ADD	R0, R0, R1	// R0 = fbp + pix_offset
-	ADD	R0, R0, #2	// R0 = fbp + pix_offset + 2
-	STRB	R2, [R0]	// R0 = r (red byte)
-	SUB	R0, R0, #1	// R0 = fbp + pix_offset + 1
-	STRB	R3, [R0]	// R0 = g (green byte)
-	SUB	R0, R0, #1	// R0 = fbp + pix_offset + 0
-	STRB	R4, [R0]	// R0 = b (blue byte)
-	MOV	PC, LR
 	
 	.text
 	.align	2
 	.global	main
 main:
-	LDR	R0, LATCH	// R0 -> .L6 -> "/dev/fb0\000"
+	//MOV	R1, #0
+	//LDR	R0, =LATCH	// R0 -> LATCH
+	//LDR	R0, [R0, +R1]
+	LDR	R0, LATCH	// R0 -> LATCH = "/dev/fb0\000"
 	MOV	R1, #2		// R1 = 2 (Opcode for O_RDWR)
 	BL	open		// Parameters: R0--R1
-	STR	R0, [SP]	// SP = open(...)
-	//NOP
-	//LDR	R0, LATCH+24	// R0 -> IMAGE = ...cyndaquil.txt
-	//MOV	R1, #2
-	//BL	open
-	//STR	R0, [SP, #20]
-	//LDR	R0, [SP]
-	//NOP
 	LDR	R1, LATCH+12	// R1 = 17920 (Opcode for FBIOGET_VSCREENINFO)
 	LDR	R2, LATCH+4	// R2 -> .L6+4 -> vinfo (Changed)
 	BL	ioctl		// Parameters: R0--R2
@@ -71,11 +44,25 @@ main:
 	BL	mmap		// Parameters: R0--R3, SP--SP+4
 	LDR	R1, LATCH+20	// R1 -> fbp
 	STR	R0, [R1]	// fbp = mmap(...)
-	LDR	R0, LATCH+20	// R0 -> LATCH+20 = fbp
-	LDR	R0, [R0]	// R0 = fbp (dereferenced)
-	LDR	R1, [SP, #8]	// SP+8 = screensize
-	BL	munmap		// Parameters: R0--R1
-	LDR	R0, [SP]	// R0 -> SP = open(...)
+	NOP
+	LDR	R0, LATCH+24	// R0 -> LATCH+24 -> IMAGE
+	MOV	R1, #2		// R1 = 2 (Opcode for O_RDWR)
+	BL	open		// Parameters: R0--R1
+	LDR	R1, [SP]	// R1 = open("/dev/fb0\000")
+	STR	R1, [SP, #12]	// SP+12 = open("/dev/fb0\000")
+	STR	R0, [SP]	// SP = open("/home/pi/Desktop/shoulder/images/image.bin\000")
+	LDR	R0, [SP, #8]	// R0 = screensize
+	LDR	R1, LATCH+28	// R1 = 250 (Height)
+	LDR	R2, LATCH+32	// R2 = 250 (Width)
+	MUL	R1, R1, R2	// R1 = Height * Width
+	MOV	R2, #4		// R2 = 4 (bytes per pixel)
+	MUL	R1, R1, R2	// R1 = Height * Width * 4
+	MOV	R2, #3		// R2 = 3 (Opcode for PROT_READ | PROT_WRITE)
+	MOV	R3, #1		// R3 = 1 (Opcode for MAP_SHARED)
+	BL	mmap		// Parameters: R0--R3, SP--SP+4
+	STR	R0, [SP, #16]	// SP+16 = mmap(...)
+	NOP
+	LDR	R0, [SP, #12]	// R0 -> SP+12 = open("/dev/fb0\000")
 	BL	close		// Parameters: R0
 	MOV	R0, #0		// R0 = 0 (return code)
 	MOVAL	R7, #1		// R7 = 1 (exit syscall)
@@ -90,6 +77,8 @@ LATCH:
 	.word	17922
 	.word	fbp
 	.word	IMAGE
+	.word	250
+	.word	250
 
 	.section	.rodata
 	.align	2
@@ -97,4 +86,4 @@ FILE:
 	.ascii	"/dev/fb0\000"
 
 IMAGE:
-	.ascii "/home/pi/Desktop/shoulder/images/cyndaquil.txt\000"
+	.ascii "/home/pi/Desktop/shoulder/images/image.bin\000"
