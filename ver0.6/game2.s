b48413c560c04aed36f1443c4031c68b17a25303
syntaxiom <smlwcg@gmail.com>
2017-10-14 23:45:53 -0700

    Use .data for read and write

--- ver0.6/game2.s
+++ ver0.6/game2.s
@@ -13,8 +13,8 @@ fbp:
 	.align	2
 	.global	put_pixel
 put_pixel:
-	MOV	R5, #4		// R5 = 4 (bytes per pixel)
-	MUL	R0, R0, R5	// R0 = x * 4
+	MOV	R5, #4		// R5 = 3 (bytes per pixel)
+	MUL	R0, R0, R5	// R0 = x * 3
 	LDR	R5, LATCH+8	// R5 -> finfo
 	LDR	R5, [R5, #44]	// R5 = fino+44 (dereferenced) ==> finfo.line_length
 	MUL	R1, R1, R5	// R1 = y * finfo.line_length
@@ -22,7 +22,13 @@ put_pixel:
 	LDR	R0, LATCH+20	// R0 -> fbp
 	LDR	R0, [R0]	// R0 = fbp (dereferenced)
 	ADD	R0, R0, R1	// R0 = fbp + pix_offset
-	STR	R2, [R0]	// fbp + pix_offet = color
+	STR	R2, [R0]	// fbp + pix_offset = color
+	MOV	PC, LR
+
+	.text
+	.align	2
+	.global	show_image
+show_image:
 	MOV	PC, LR
 	
 	.text
@@ -58,17 +64,30 @@ main:
 	BL	mmap		// Parameters: R0--R3, SP--SP+4
 	LDR	R1, LATCH+20	// R1 -> fbp
 	STR	R0, [R1]	// fbp = mmap(...)
-	MOV	R0, #800	// x
-	MOV	R1, #800	// y
-	LDR	R2, =0x00FF0000	// color
-	BL	put_pixel	// Parameters: R0--R4
+	NOP
+	LDR	R0, IMAGES	// R0 -> IMAGES -> TITLE
+	MOV	R1, #2		// R1 = 2 (Opcode for O_RDWR)
+	BL	open		// Parameters: R0--R1
+	STR	R0, [SP, #12]	// SP+12 = open(...)
+	LDR	R0, IMAGES+4	// R0 = Height
+	LDR	R1, IMAGES+8	// R1 = Width
+	MUL	R0, R0, R1	// R0 = Height * Width
+	MOV	R1, #4		// R1 = 4
+	MUL	R0, R0, R1	// R0 = Height * Width * 4
+	LDR	R1, =LENGTH
+	STR	R0, [R1]
+	NOP
 	LDR	R0, LATCH+20	// R0 -> LATCH+20 = fbp
 	LDR	R0, [R0]	// R0 = fbp (dereferenced)
 	LDR	R1, [SP, #8]	// SP+8 = screensize
 	BL	munmap		// Parameters: R0--R1
 	LDR	R0, [SP]	// R0 -> SP = open(...)
 	BL	close		// Parameters: R0
-	MOV	R0, #0		// R0 = 0 (return code)
+	NOP
+	LDR	R0, [SP, #12]	// R0 -> SP+12 = open(...)
+	BL	close		// Parameters: R0
+	NOP
+	MOVAL	R0, #0		// R0 = 0 (return code)
 	MOVAL	R7, #1		// R7 = 1 (exit syscall)
 	SWI	0
 
@@ -80,8 +99,24 @@ LATCH:
 	.word	17920
 	.word	17922
 	.word	fbp
+	.word	0
+
+IMAGES:
+	.word	TITLE
+	.word	250
+	.word	250
+
+	.data
+LENGTH:
+	.word	0
+
+BUFFER:
+	.word	0
 
 	.section	.rodata
 	.align	2
 FILE:
 	.ascii	"/dev/fb0\000"
+	
+TITLE:
+	.ascii	"/home/pi/Desktop/image.bin\000"
