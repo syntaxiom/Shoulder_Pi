1ebea434c86c2afe7c39f3380941a0b54dcccad2
syntaxiom <smlwcg@gmail.com>
2017-10-16 00:34:09 -0700

    Slight optimizations before moving forward

--- ver0.6/game7.s
+++ ver0.6/game7.s
@@ -13,11 +13,10 @@ fbp:
 	.align	2
 	.global	put_pixel
 put_pixel:
-	MOV	R5, #4		// R5 = 4 (bytes per pixel)
-	MUL	R0, R0, R5	// R0 = x * 4
-	LDR	R5, LATCH+8	// R5 -> finfo
-	LDR	R5, [R5, #44]	// R5 = fino+44 (dereferenced) ==> finfo.line_length
-	MUL	R1, R1, R5	// R1 = y * finfo.line_length
+	MOV	R3, #4		// R3 = 4 (bytes per pixel)
+	MUL	R0, R0, R3	// R0 = x * 4
+	LDR	R3, [SP, #12]	// R3 = finfo.line_length
+	MUL	R1, R1, R3	// R1 = y * finfo.line_length
 	ADD	R1, R0, R1	// R1 = x * 3 + y * finfo.line_length = pix_offset
 	LDR	R0, LATCH+20	// R0 -> fbp
 	LDR	R0, [R0]	// R0 = fbp (dereferenced)
@@ -65,15 +64,19 @@ main:
 	LDR	R1, LATCH+20	// R1 -> fbp
 	STR	R0, [R1]	// fbp = mmap(...)
 	NOP
+	LDR	R0, LATCH+8	// R0 -> finfo
+	LDR	R0, [R0, #44]	// R0 = finfo+44 (dereferenced) ==> finfo.line_length
+	STR	R0, [SP, #12]	// SP+12 = finfo.line_length
+	NOP
 	LDR	R0, IMAGES	// R0 -> IMAGES -> TITLE
 	MOV	R1, #2		// R1 = 2 (Opcode for O_RDWR)
 	BL	open		// Parameters: R0--R1
-	STR	R0, [SP, #12]	// SP+12 = open(...)
-	LDR	R1, =BUFFER	// R1 -> BUFFER
-	MOV	R2, #4		// R2 = 4 (bytes to read)
-	BL	read		// Parameters: R0--R2
-	LDR	R0, =BUFFER	// R0 -> BUFFER
-	LDR	R0, [R0]	// R0 = BUFFER (dereferenced)
+	STR	R0, [SP, #16]	// SP+12 = open(...)
+	NOP
+	MOV	R0, #800
+	MOV	R1, #800
+	LDR	R2, =0x00FF0000
+	BL	put_pixel
 	NOP
 	LDR	R0, LATCH+20	// R0 -> LATCH+20 = fbp
 	LDR	R0, [R0]	// R0 = fbp (dereferenced)
@@ -81,10 +84,8 @@ main:
 	BL	munmap		// Parameters: R0--R1
 	LDR	R0, [SP]	// R0 -> SP = open(...)
 	BL	close		// Parameters: R0
-	NOP
-	LDR	R0, [SP, #12]	// R0 -> SP+12 = open(...)
+	LDR	R0, [SP, #16]	// R0 -> SP+12 = open(...)
 	BL	close		// Parameters: R0
-	NOP
 	MOVAL	R0, #0		// R0 = 0 (return code)
 	MOVAL	R7, #1		// R7 = 1 (exit syscall)
 	SWI	0
@@ -99,14 +100,10 @@ LATCH:
 	.word	fbp
 IMAGES:
 	.word	TITLE
-	.word	250
-	.word	250
 
 	.data
-LENGTH:
-	.word	0
 BUFFER:
-	.skip	4
+	.skip	12
 
 	.section	.rodata
 	.align	2
