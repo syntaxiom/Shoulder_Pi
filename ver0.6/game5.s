7cbe45f667e5c8c7a6b40292169a6f6a2b2dd359
syntaxiom <smlwcg@gmail.com>
2017-10-15 21:40:36 -0700

    Let's go, game5

---
+++ ver0.6/game5.s
@@ -0,0 +1,116 @@
+	.global	fbp
+	.bss
+	.align	2
+	.type	fbp, %object
+	.size	fbp, 4
+fbp:
+	.space	4
+	.comm	vinfo,160,4
+	.comm	finfo,68,4
+
+	/* R0 = x, R1 = y, R2 = color */
+	.text
+	.align	2
+	.global	put_pixel
+put_pixel:
+	MOV	R5, #4		// R5 = 4 (bytes per pixel)
+	MUL	R0, R0, R5	// R0 = x * 4
+	LDR	R5, LATCH+8	// R5 -> finfo
+	LDR	R5, [R5, #44]	// R5 = fino+44 (dereferenced) ==> finfo.line_length
+	MUL	R1, R1, R5	// R1 = y * finfo.line_length
+	ADD	R1, R0, R1	// R1 = x * 3 + y * finfo.line_length = pix_offset
+	LDR	R0, LATCH+20	// R0 -> fbp
+	LDR	R0, [R0]	// R0 = fbp (dereferenced)
+	ADD	R0, R0, R1	// R0 = fbp + pix_offset
+	STR	R2, [R0]	// fbp + pix_offset = color
+	MOV	PC, LR
+
+	.text
+	.align	2
+	.global	show_image
+show_image:
+	MOV	PC, LR
+	
+	.text
+	.align	2
+	.global	main
+main:
+	LDR	R0, LATCH	// R0 -> LATCH -> "/dev/fb0\000"
+	MOV	R1, #2		// R1 = 2 (Opcode for O_RDWR)
+	BL	open		// Parameters: R0--R1
+	STR	R0, [SP]	// SP = open(...)
+	LDR	R1, LATCH+12	// R1 = 17920 (Opcode for FBIOGET_VSCREENINFO)
+	LDR	R2, LATCH+4	// R2 -> LATCH+4 -> vinfo (Changed)
+	BL	ioctl		// Parameters: R0--R2
+	LDR	R0, [SP]     	// R0 -> SP = open(...);
+	LDR	R1, LATCH+16	// R1 = 17922 (Opcode for FBIOGET_FSCREENINFO)
+	LDR	R2, LATCH+8	// R2 -> LATCH+8 -> finfo
+	BL	ioctl		// Parameters: R0--R2
+	LDR	R0, LATCH+4	// R0 -> LATCH+4 -> vinfo
+	LDR	R0, [R0]	// R0 = vinfo+0 (dereferenced) ==> vinfo.xres
+	LDR	R1, LATCH+4	// R1 -> LATCH+4 -> vinfo
+	LDR	R1, [R1, #4]	// R1 = vinfo+4 (dereferenced) ==> vinfo.yres
+	LDR	R2, LATCH+4	// R2 -> LATCH+4 -> vinfo
+	LDR	R2, [R2, #24]	// R2 = vinfo+24 (dereferenced) ==> vinfo.bits_per_pixel
+	MUL	R1, R0, R1	// R1 = vinfo.xres * vinfo.yres
+	MUL	R2, R1, R2	// R2 = R1 * vinfo.bits_per_pixel
+	LSR	R2, R2, #3	// R2 /= 8
+	STR	R2, [SP, #8]	// SP+8 = screensize
+	MOV	R0, #0		// R0 = 0
+	LDR	R1, [SP, #8]	// SP+8 = screensize
+	MOV	R2, #3		// R2 = 3 (Opcode for PROT_READ | PROT_WRITE)
+	MOV	R3, #1		// R3 = 1 (Opcode for MAP_SHARED)
+	STR	R0, [SP, #4]	// SP+4 = 0
+	BL	mmap		// Parameters: R0--R3, SP--SP+4
+	LDR	R1, LATCH+20	// R1 -> fbp
+	STR	R0, [R1]	// fbp = mmap(...)
+	NOP
+	LDR	R0, IMAGES	// R0 -> IMAGES -> TITLE
+	MOV	R1, #2		// R1 = 2 (Opcode for O_RDWR)
+	BL	open		// Parameters: R0--R1
+	STR	R0, [SP, #12]	// SP+12 = open(...)
+	LDR	R1, =BUFFER	// R1 -> BUFFER
+	MOV	R2, #4		// R2 = 4 (bytes to read)
+	BL	read		// Parameters: R0--R2
+	LDR	R0, =BUFFER	// R0 -> BUFFER
+	LDR	R0, [R0]	// R0 = BUFFER (dereferenced)
+	NOP
+	LDR	R0, LATCH+20	// R0 -> LATCH+20 = fbp
+	LDR	R0, [R0]	// R0 = fbp (dereferenced)
+	LDR	R1, [SP, #8]	// SP+8 = screensize
+	BL	munmap		// Parameters: R0--R1
+	LDR	R0, [SP]	// R0 -> SP = open(...)
+	BL	close		// Parameters: R0
+	NOP
+	LDR	R0, [SP, #12]	// R0 -> SP+12 = open(...)
+	BL	close		// Parameters: R0
+	NOP
+	MOVAL	R0, #0		// R0 = 0 (return code)
+	MOVAL	R7, #1		// R7 = 1 (exit syscall)
+	SWI	0
+
+	.align	2
+LATCH:
+	.word	FRAMEBUFFER
+	.word	vinfo
+	.word	finfo
+	.word	17920
+	.word	17922
+	.word	fbp
+IMAGES:
+	.word	TITLE
+	.word	250
+	.word	250
+
+	.data
+LENGTH:
+	.word	0
+BUFFER:
+	.skip	4
+
+	.section	.rodata
+	.align	2
+FRAMEBUFFER:
+	.ascii	"/dev/fb0\000"
+TITLE:
+	.ascii	"/home/pi/Desktop/image.bin\000"
