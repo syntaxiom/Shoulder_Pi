fa4830c30d8243391cd93f05497e747ba67296e9
syntaxiom <smlwcg@gmail.com>
2017-12-04 20:55:40 -0800

    Final push before stuff breaks

--- ver0.9/final5.s
+++ ver0.9/final5.s
@@ -1,3 +1,5 @@
+	.fpu neon
+
 	.global	fbp
 	.bss
 	.align	2
@@ -23,36 +25,36 @@ put_screen:
 	
 	.global	main
 main:
-	LDR	R0, LATCH	// R0 -> LATCH -> "/dev/fb0\000"
+	LDR	R0, =FRAMEBUF	// R0 -> FRAMEBUF
 	MOV	R1, #2		// R1 = 2 (Opcode for O_RDWR)
 	BL	open		// Parameters: R0--R1
 	LDR	R1, =FB_FILED	// R1 -> FB_FILED
 	STR	R0, [R1]	// FB_FILED = open("/dev/fb0\000")
 	STR	R0, [SP]	// SP = open("/dev/fb0\000")
-	LDR	R1, LATCH+12	// R1 = 17920 (Opcode for FBIOGET_VSCREENINFO)
-	LDR	R2, LATCH+4	// R2 -> vinfo
+	LDR	R1, =17920	// R1 = 17920 (Opcode for FBIOGET_VSCREENINFO)
+	LDR	R2, =vinfo	// R2 -> vinfo
 	BL	ioctl		// Parameters: R0--R2
 	LDR	R0, [SP]	// R0 = open("/dev/fb0\000")
-	LDR	R1, LATCH+16	// R1 = 17922 (Opcode for FBIOGET_FSCREENINFO)
-	LDR	R2, LATCH+8	// R2 -> finfo
+	LDR	R1, =17922	// R1 = 17922 (Opcode for FBIOGET_FSCREENINFO)
+	LDR	R2, =finfo	// R2 -> finfo
 	BL	ioctl		// Parameters: R0--R2
-	LDR	R0, LATCH+4	// R0 -> vinfo
+	LDR	R0, =vinfo	// R0 -> vinfo
 	LDR	R1, [R0, #0]	// R1 = vinfo.xres
 	LSL	R1, R1, #2	// R1 = vinfo.xres * 4 = line length
-	LDR	R2, LATCH+28	// R2 -> LINELENGTH
+	LDR	R2, =LINELENGTH	// R2 -> LINELENGTH
 	STR	R1, [R2]	// LINELENGTH = line length
 	LDR	R2, [R0, #4]	// R2 = vinfo.yres
 	MUL	R1, R1, R2	// R1 = line length * vinfo.yres
-	LDR	R0, LATCH+24	// R0 -> screen size
+	LDR	R0, =SCREENSIZE	// R0 -> screen size
 	STR	R1, [R0]	// screen size = vinfo.xres * vinfo.yres * 4
 	MOV	R0, #0		// R0 = 0
-	LDR	R1, LATCH+24	// R1 -> screen size
+	LDR	R1, =SCREENSIZE	// R1 -> screen size
 	LDR	R1, [R1]	// R1 = screen size
 	MOV	R2, #3		// R2 = 3 (Opcode for PROT_READ | PROT_WRITE)
 	MOV	R3, #1		// R3 = 1 (Opcode for MAP_SHARED)
 	STR	R0, [SP, #4]	// SP+4 = 0
 	BL	mmap		// Parameters: R0--R3, SP--SP+4
-	LDR	R1, LATCH+20	// R1 -> fbp
+	LDR	R1, =fbp	// R1 -> fbp
 	STR	R0, [R1]	// fbp = mmap(...)
 
 open_image:
@@ -61,7 +63,7 @@ open_image:
 	BL	open		// Parameters: R0--R1
 	LDR	R1, =IMG_FILED	// R1 -> IMG_FILED
 	STR	R0, [R1]	// IMG_FILED = open("/home/pi/Desktop/image.bin\000")
-	MOV	R4, #0		// R4 = 0
+	MOV	R4, #0		// R4 = 0 (track size of stack)
 
 image_loop:
 	LDR	R0, =IMG_FILED	// R0 -> IMG_FILED
@@ -70,7 +72,7 @@ image_loop:
 	MOV	R2, #12		// R2 = 12 (bytes to read)
 	BL	read		// Parameters: R0--R2
 	CMP	R0, #0		// R0 ? 0
-	BEQ	stuff		// (Break)
+	BEQ	set_stack	// (Break)
 	LDR	R0, =POSCOLOR	// R0 -> POSCOLOR
 	LDR	R1, [R0, #0]	// R1 = x
 	LDR	R2, [R0, #4]	// R2 = y
@@ -82,10 +84,16 @@ image_loop:
 	ADD	R1, R1, R2	// R1 = (x * 4) + (y * LINELENGTH) (offset)
 	LDR	R0, =BUFFER	// R0 -> BUFFER
 	STR	R3, [R0, R1]	// BUFFER[offset] = color
-	PUSH	{R1, R3}	// Push offset, color
+	PUSH	{R1, R3}	// Push {offset, color}
+	ADD	R4, #8		// R4 += 8
 	BAL	image_loop	// (Loop)
 
-stuff:
+set_stack:
+	LDR	R0, =STACKSIZE	// R0 -> STACKSIZE
+	STR	R4, [R0]	// STACKSIZE = R4
+	MOV	FP, SP		// Set dynamic link
+
+set_screen:
 	LDR	R0, =BUFFER	// R0 -> BUFFER
 	LDR	R1, =fbp	// R1 -> fbp
 	LDR	R1, [R1]	// R1 = fbp
@@ -96,8 +104,8 @@ stuff:
 	BL	put_screen	// Parameters: R0--R2
 
 done:
-	LDR	R0, LATCH+20	// R0 -> fbp
-	LDR	R1, LATCH+24	// R1 -> screen size
+	LDR	R0, =fbp	// R0 -> fbp
+	LDR	R1, =SCREENSIZE	// R1 -> screen size
 	LDR	R1, [R1]	// R1 = screen size
 	BL	munmap		// Parameters: R0--R1
 	LDR	R0, =FB_FILED	// R0 -> FB_FILED
@@ -108,20 +116,6 @@ done:
 	BL	close		// Parameters: R0
 	MOV	R0, #0		// R0 = 0 (return code)
 	BLAL	exit		// Terminate the program
-	
-LATCH:
-	.word	FRAMEBUFFER
-	.word	vinfo
-	.word	finfo
-	.word	17920
-	.word	17922
-	.word	fbp
-	.word	SCREENSIZE
-	.word	LINELENGTH
-	.word	delay
-IMAGE:
-	.word	IMAGEFILE
-	.word	COORDS
 
 	.bss
 BUFFER:
@@ -140,13 +134,14 @@ SCREENSIZE:
 	.word	0
 LINELENGTH:
 	.word	0
-COORDS:
-	.word	0x0200
-	.word	0x0100
 STACKSIZE:
 	.word	0
+XPOS:
+	.word	200
+YPOS:
+	.word	100
 	
-FRAMEBUFFER:
+FRAMEBUF:
 	.ascii	"/dev/fb0\000"
 IMAGEFILE:
 	.ascii	"/home/pi/Desktop/image.bin\000"
